10cf3db9e383b6fd029fa719436757455cc403c5
shashi-dokania <shashi.dokania@gmail.com>
2015-11-02 21:34:54 -0800

    implemented custom session checker middleware :)

---
+++ db/custom_middleware/session-checker.js
@@ -0,0 +1,11 @@
+module.exports = function(args){
+  var loginUrl = args.loginUrl !== undefined ? args.loginUrl : '/login';
+
+  return function(req, res, next){
+    if(req.session.user === undefined ){
+      req.url = loginUrl;
+    }
+    next();
+  };
+
+};
